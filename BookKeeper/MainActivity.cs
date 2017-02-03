e05a480a78e6b49fa56fcf5652ad3c692e167467
tobiashillen <tobias.hillen@gmail.com>
2017-02-03 14:04:11 +0100

    Did som refactoring in main.

--- BookKeeper/MainActivity.cs
+++ BookKeeper/MainActivity.cs
@@ -15,7 +15,6 @@ namespace BookKeeper
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.activity_main);
 
 
